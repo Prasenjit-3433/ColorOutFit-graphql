3f40dcea5df983f51c90f0f43fde3ec17f86f208
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2022-12-19 21:56:59 +0530

    Updating Components To Consume GraphQL Values

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,6 +1,7 @@
 import { useContext, useState, useEffect, Fragment } from 'react';
 import { useParams } from 'react-router-dom';
 
+import Spinner from '../../components/spinner/spinner.component';
 import ProductCard from '../../components/product-card/product-card.component';
 
 import { CategoriesContext } from '../../contexts/categories.context';
@@ -9,7 +10,7 @@ import { CategoryContainer, Title } from './category.styles';
 
 const Category = () => {
   const { category } = useParams();
-  const { categoriesMap } = useContext(CategoriesContext);
+  const { categoriesMap, loading } = useContext(CategoriesContext);
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
@@ -17,7 +18,9 @@ const Category = () => {
   }, [category, categoriesMap]);
 
   return (
-    <Fragment>
+    loading ?
+    <Spinner/> :
+    (<Fragment>
       <Title>{category.toUpperCase()}</Title>
       <CategoryContainer>
         {products &&
@@ -25,7 +28,7 @@ const Category = () => {
             <ProductCard key={product.id} product={product} />
           ))}
       </CategoryContainer>
-    </Fragment>
+    </Fragment>)
   );
 };
 
